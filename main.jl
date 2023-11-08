5762305859a1f0dc60381b3da32a90be59ff9631
Shoram444 <petro.maros.afs@gmail.com>
2023-11-08 15:42:32 +0100

    Added stoB calculation

--- main.jl
+++ main.jl
@@ -35,20 +35,31 @@ SNparams = Dict(
 # generate_raw_plots(Pa234m, "Pa234m")
 # generate_raw_plots(bb, "2nubb")
 
+
+
+###########################################
+###########################################
+###########################################
+###########################################
+###########################################
 #### Sum energy spectra
 sumEParams = Dict(
     :binning => 0:100:3500,
     :nTotalSim => 1e8
 )
 
+BkgActivityParams = Dict( #activities from Table 1 from 10.1140/epjc/s10052-018-6295-x
+    :Bi214 => 1.50 / 1000 * SNparams["m"], # [mBq/kg] converted to [Bq]
+    :Tl208 => 0.39 / 1000 * SNparams["m"], # [mBq/kg] converted to [Bq]
+    :Pa234m => 17.3 / 1000 * SNparams["m"], # [mBq/kg] converted to [Bq]
+    :K40 => 58.7 / 1000 * SNparams["m"] # [mBq/kg] converted to [Bq]
+)
 
-Bi214SumE    = Process(Bi214.reconstructedEnergy1 .+ Bi214.reconstructedEnergy2, "Bi214", :false, 0.1, SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
-Tl208SumE    = Process(Tl208.reconstructedEnergy1 .+ Tl208.reconstructedEnergy2, "Tl208", :false, 0.1, SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
-K40SumE      = Process(K40.reconstructedEnergy1 .+ K40.reconstructedEnergy2, "K40", :false, 0.1, SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
-Pa234mSumE   = Process(Pa234m.reconstructedEnergy1 .+ Pa234m.reconstructedEnergy2, "Pa234m", :false, 0.1, SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
+Bi214SumE    = Process(Bi214.reconstructedEnergy1 .+ Bi214.reconstructedEnergy2, "Bi214", :false, BkgActivityParams[:Bi214], SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
+Tl208SumE    = Process(Tl208.reconstructedEnergy1 .+ Tl208.reconstructedEnergy2, "Tl208", :false, BkgActivityParams[:Tl208], SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
+K40SumE      = Process(K40.reconstructedEnergy1 .+ K40.reconstructedEnergy2, "K40", :false, BkgActivityParams[:K40], SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
+Pa234mSumE   = Process(Pa234m.reconstructedEnergy1 .+ Pa234m.reconstructedEnergy2, "Pa234m", :false, BkgActivityParams[:Pa234m], SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
 bbSumE       = Process(bb.reconstructedEnergy1 .+ bb.reconstructedEnergy2, "2nubb", :true, 1, SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
-
-savename(Bi214SumE, allowedtypes = (String, StepRange), "png")
 
 let 
     default(xlabel = "min sum energy [keV]", ylabel = "max sum energy [keV]", c = :coolwarm, size = (1600, 1000), thickness_scaling = 1.4, right_margin = 16Plots.mm)
@@ -73,18 +84,71 @@ let
     # $(bbSumE.isotopeName)_phi$(step(sumEParams[:binning]))-degree_binning.png")
 end
 
+stbSum = @show get_sToBRatio(bbSumE, Bi214SumE, Tl208SumE, Pa234mSumE)
+best_stbSum= maximum(stbSum) |> round
+minSumE, maxSumE = bbSumE.bins[argmax(stbSum)[1]], bbSumE.bins[argmax(stbSum)[2]]
+plot_sToBRatio(bbSumE, Bi214SumE, Tl208SumE, Pa234mSumE; c=:coolwarm, title ="signal to background ratio for SumE channel 2nubb \nmax_stb = $best_stbSum at ($minSumE - $maxSumE)keV")
+savefig(plotsdir(savename("StoB_SumE_binning",bbSumE.bins,"png")))
 
-plot_sToBRatio(bbSumE, Bi214SumE, Tl208SumE, Pa234mSumE; c=:coolwarm)
-@show get_sToBRatio(bbSumE, Bi214SumE, Tl208SumE, Pa234mSumE)
 
+###########################################
+###########################################
+###########################################
+###########################################
+###########################################
+#### Single energy spectra
+singleEParams = Dict(
+    :binning => 0:100:3500,
+    :nTotalSim => 2e8 # because we have 2 electrons per event
+)
 
-get_bkg_counts(bbSumE, Bi214SumE, Tl208SumE,)
+BkgActivityParams = Dict( #activities from Table 1 from 10.1140/epjc/s10052-018-6295-x
+    :Bi214 => 1.50 / 1000 * SNparams["m"], # [mBq/kg] converted to [Bq]
+    :Tl208 => 0.39 / 1000 * SNparams["m"], # [mBq/kg] converted to [Bq]
+    :Pa234m => 17.3 / 1000 * SNparams["m"], # [mBq/kg] converted to [Bq]
+    :K40 => 58.7 / 1000 * SNparams["m"] # [mBq/kg] converted to [Bq]
+)
 
-lookup_efficiency(bbPhi, 0, 179)
-plot_efficiency(bbPhi, bbPhi.bins)
+Bi214SingleE    = Process(vcat(Bi214.reconstructedEnergy1,  Bi214.reconstructedEnergy2), "Bi214", :false, BkgActivityParams[:Bi214], SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
+Tl208SingleE    = Process(vcat(Tl208.reconstructedEnergy1,  Tl208.reconstructedEnergy2), "Tl208", :false, BkgActivityParams[:Tl208], SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
+K40SingleE      = Process(vcat(K40.reconstructedEnergy1,  K40.reconstructedEnergy2), "K40", :false, BkgActivityParams[:K40], SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
+Pa234mSingleE   = Process(vcat(Pa234m.reconstructedEnergy1,  Pa234m.reconstructedEnergy2), "Pa234m", :false, BkgActivityParams[:Pa234m], SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
+bbSingleE       = Process(vcat(bb.reconstructedEnergy1,  bb.reconstructedEnergy2), "2nubb", :true, 1, SNparams["t"], sumEParams[:nTotalSim], sumEParams[:binning])
 
-bbPhi.efficiency
+let 
+    default(xlabel = "min single energy [keV]", ylabel = "max single energy [keV]", c = :coolwarm, size = (1600, 1000), thickness_scaling = 1.4, right_margin = 16Plots.mm)
+    plot_efficiency(Bi214SingleE, singleEParams[:binning], title ="single E Efficiency for "* Bi214SingleE.isotopeName)
+    savefig(plotsdir(savename("efficiency_singleE_", Bi214SingleE, allowedtypes = (String, StepRange), "png")))
+    # $(Bi214SingleE.isotopeName)_$(step(singleEParams[:binning]))-degree_binning.png")
 
+    plot_efficiency(Tl208SingleE, singleEParams[:binning], title ="single E Efficiency for "* Tl208SingleE.isotopeName)
+    savefig(plotsdir(savename("efficiency_singleE_", Tl208SingleE, allowedtypes = (String, StepRange), "png")))
+    # $(Tl208SingleE.isotopeName)_$(step(singleEParams[:binning]))-degree_binning.png")
+
+    plot_efficiency(K40SingleE, singleEParams[:binning], title ="single E Efficiency for "* K40SingleE.isotopeName)
+    savefig(plotsdir(savename("efficiency_singleE_", K40SingleE, allowedtypes = (String, StepRange), "png")))
+    # $(K40SingleE.isotopeName)_ph$(step(singleEParams[:binning]))-degree_binning.png")
+
+    plot_efficiency(Pa234mSingleE, singleEParams[:binning], title ="single E Efficiency for "* Pa234mSingleE.isotopeName)
+    savefig(plotsdir(savename("efficiency_singleE_", Pa234mSingleE, allowedtypes = (String, StepRange), "png")))
+    # $(Pa234mSingleE.isotopeName)_phi$(step(singleEParams[:binning]))-degree_binning.ng"))
+
+    plot_efficiency(bbSingleE, singleEParams[:binning], title ="single E Efficiency for "* bbSingleE.isotopeName)
+    savefig(plotsdir(savename("efficiency_singleE_", bbSingleE, allowedtypes = (String, StepRange), "png")))
+    # $(bbSingleE.isotopeName)_phi$(step(singleEParams[:binning]))-degree_binning.png")
+end
+
+stbSingle = @show get_sToBRatio(bbSingleE, Bi214SingleE, Tl208SingleE, Pa234mSingleE)
+best_stbSingle= maximum(stbSingle) |> round
+minSingleE, maxSingleE = bbSingleE.bins[argmax(stbSingle)[1]], bbSingleE.bins[argmax(stbSingle)[2]]
+plot_sToBRatio(bbSingleE, Bi214SingleE, Tl208SingleE, Pa234mSingleE; c=:coolwarm, title ="signal to background ratio for 2nubb \nmax_stb = $best_stbSingle at ($minSingleE - $maxSingleE)keV")
+savefig(plotsdir(savename("StoB_singleE_binning",bbSingleE.bins,"png")))
+
+###########################################
+###########################################
+###########################################
+###########################################
+###########################################
 #### Angular Study
 phiParams = Dict(
     :binning => 0:5:180,
@@ -92,10 +156,10 @@ phiParams = Dict(
 )
 
 
-Bi214Phi    = Process(Bi214.phi, "Bi214", :false, 0.1, SNparams["t"], phiParams[:nTotalSim], phiParams[:binning])
-Tl208Phi    = Process(Tl208.phi, "Tl208", :false, 0.1, SNparams["t"], phiParams[:nTotalSim], phiParams[:binning])
-K40Phi      = Process(K40.phi, "K40", :false, 0.1, SNparams["t"], phiParams[:nTotalSim], phiParams[:binning])
-Pa234mPhi   = Process(Pa234m.phi, "Pa234m", :false, 0.1, SNparams["t"], phiParams[:nTotalSim], phiParams[:binning])
+Bi214Phi    = Process(Bi214.phi, "Bi214", :false, BkgActivityParams[:Bi214], SNparams["t"], phiParams[:nTotalSim], phiParams[:binning])
+Tl208Phi    = Process(Tl208.phi, "Tl208", :false, BkgActivityParams[:Tl208], SNparams["t"], phiParams[:nTotalSim], phiParams[:binning])
+K40Phi      = Process(K40.phi, "K40", :false, BkgActivityParams[:K40], SNparams["t"], phiParams[:nTotalSim], phiParams[:binning])
+Pa234mPhi   = Process(Pa234m.phi, "Pa234m", :false, BkgActivityParams[:Pa234m], SNparams["t"], phiParams[:nTotalSim], phiParams[:binning])
 bbPhi       = Process(bb.phi, "2nubb", :true, 1, SNparams["t"], phiParams[:nTotalSim], phiParams[:binning])
 
 
@@ -118,10 +182,9 @@ let
     savefig(joinpath(plotsdir(), "efficiency_anglular_$(Pa234mPhi.isotopeName)_phi$(step(phiParams[:binning]))-degree_binning.png"))
 end
 
+stbPhi = @show get_sToBRatio(bbPhi, Bi214Phi, Tl208Phi, Pa234mPhi)
+best_stbPhi= maximum(stbPhi) |> round
+minPhi, maxPhi = bbPhi.bins[argmax(stbPhi)[1]], bbPhi.bins[argmax(stbPhi)[2]]
+plot_sToBRatio(bbPhi, Bi214Phi, Tl208Phi, Pa234mPhi; c=:coolwarm, title ="signal to background ratio for angular for 2nubb \nmax_stb = $best_stbPhi at ($minPhi - $maxPhi)°")
+savefig(plotsdir(savename("StoB_Angular_binning",bbPhi.bins,"png")))
 
-plot_sToBRatio(bbPhi, Bi214Phi, Tl208Phi; c=:coolwarm)
-
-lookup_efficiency(bbPhi, 0, 179)
-plot_efficiency(bbPhi, bbPhi.bins)
-
-bbPhi.efficiency
--- src/Process.jl
+++ src/Process.jl
@@ -100,6 +100,25 @@ function get_nPassed(process::Process, bins; kwargs...)
     h2d.weights
 end
 
+function get_nPassed(process::Process; kwargs...)
+    h1d = StatsBase.fit(Histogram{Float64}, process.dataVector, process.bins)
+
+    h2d = StatsBase.fit(Histogram{Float64}, (Float64[], Float64[]), (process.bins,process.bins)) #prepare empty 2d Histogram
+
+    minBin, stepBin, maxBin = process.bins[1], step(process.bins), process.bins[end-1]
+
+    for minVal in minBin:stepBin:maxBin-stepBin
+        for maxVal in minVal+stepBin:stepBin:maxBin
+            minBinID = FHist._edge_binindex(process.bins, minVal)
+            maxBinID = FHist._edge_binindex(process.bins, maxVal)
+            binSum = sum(h1d.weights[Int(minBinID):Int(maxBinID)]) # integrate the histogram from minimal bin to maximal
+
+            push!(h2d, (minVal, maxVal), binSum)
+        end
+    end
+    h2d.weights
+end
+
 function plot_nPassed(process::Process, bins; kwargs...)
     h1d = StatsBase.fit(Histogram{Float64}, process.dataVector, bins)
 
@@ -119,12 +138,44 @@ function plot_nPassed(process::Process, bins; kwargs...)
     plot(h2d; kwargs...)
 end
 
+function plot_nPassed(process::Process; kwargs...)
+    h1d = StatsBase.fit(Histogram{Float64}, process.dataVector, process.bins)
+
+    h2d = StatsBase.fit(Histogram{Float64}, (Float64[], Float64[]), (process.bins,process.bins)) #prepare empty 2d Histogram
+
+    minBin, stepBin, maxBin = process.bins[1], step(process.bins), process.bins[end-1]
+
+    for minVal in minBin:stepBin:maxBin-stepBin
+        for maxVal in minVal+stepBin:stepBin:maxBin
+            minBinID = FHist._edge_binindex(process.bins, minVal)
+            maxBinID = FHist._edge_binindex(process.bins, maxVal)
+            binSum = sum(h1d.weights[Int(minBinID):Int(maxBinID)]) # integrate the histogram from minimal bin to maximal
+
+            push!(h2d, (minVal, maxVal), binSum)
+        end
+    end
+    plot(h2d; kwargs...)
+end
+
 function lookup_efficiency(process::Process, x, y)
     effMatrix = process.efficiency
-    @show binning = process.bins
+    binning = process.bins
     rx, ry = binning, binning
     !(first(rx) <= x <= last(rx)) && return missing
     !(first(ry) <= y <= last(ry)) && return missing
+    @show binning[FHist._edge_binindex(rx, x)], binning[FHist._edge_binindex(ry, y)]
+    @show FHist._edge_binindex(rx, x), FHist._edge_binindex(ry, y)
+    return effMatrix[FHist._edge_binindex(rx, x), FHist._edge_binindex(ry, y)]
+end
+
+function lookup_efficiency(h::Histogram, x, y)
+    effMatrix = h.weights
+    binning = h.edges[1]
+    rx, ry = binning, binning
+    !(first(rx) <= x <= last(rx)) && return missing
+    !(first(ry) <= y <= last(ry)) && return missing
+    @show binning[FHist._edge_binindex(rx, x)], binning[FHist._edge_binindex(ry, y)]
+    @show FHist._edge_binindex(rx, x), FHist._edge_binindex(ry, y)
     return effMatrix[FHist._edge_binindex(rx, x), FHist._edge_binindex(ry, y)]
 end
 
@@ -167,7 +218,7 @@ function get_sToBRatio(processes::Process...)
 
     sToB = zeros(size(signalCounts)) # initialize empty
     for i in eachindex(signalCounts)
-        sToB[i] = signalCounts[i] / backgroundCounts[i] # in each cell divide signal by bkg
+        !(backgroundCounts[i]==0.0) && (sToB[i] = signalCounts[i] / backgroundCounts[i]) # in each cell divide signal by bkg if bkg ≠ 0
     end
     
     return sToB
