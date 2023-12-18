3aab8e8ddaaa2e8761fe3148c354211ef8ea64fd
Shoram444 <petro.maros.afs@gmail.com>
2023-12-18 10:50:40 +0100

    Added Bi from wires to vertexSeparation

--- scripts/VertexSeparation.jl
+++ scripts/VertexSeparation.jl
@@ -7,18 +7,22 @@ Revise.track(SensitivityModule)
 
 include(scriptsdir("Params.jl"))
 
-Bi214file = ROOTFile("data/sims/Bi214_EnePhiDistPos_J23.root")
-Tl208file = ROOTFile("data/sims/Tl208_EnePhiDistPos_J23.root")
-K40file = ROOTFile("data/sims/K40_EnePhiDistPos_J23.root")
-Pa234mfile = ROOTFile("data/sims/Pa234m_EnePhiDistPos_J23.root")
-bbfile = ROOTFile("data/sims/2nubb_EnePhiDistPos_J23.root")   # Standard 2nubb
+Bi214File = ROOTFile("data/sims/Bi214_EnePhiDistPos_J23.root")
+Bi214SurfFile = ROOTFile("data/sims/Bi214_wire_surface_EnePhiDistPos_J23.root")
+Bi214BulkFile = ROOTFile("data/sims/Bi214_wire_bulk_EnePhiDistPos_J23.root")
+Tl208File = ROOTFile("data/sims/Tl208_EnePhiDistPos_J23.root")
+K40File = ROOTFile("data/sims/K40_EnePhiDistPos_J23.root")
+Pa234mFile = ROOTFile("data/sims/Pa234m_EnePhiDistPos_J23.root")
+bbFile = ROOTFile("data/sims/2nubb_EnePhiDistPos_J23.root")   # Standard 2nubb
 
 
-Bi214 = fill_from_root_file(Bi214file, "tree", keys(Bi214file["tree"]))     |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
-Tl208 = fill_from_root_file(Tl208file, "tree", keys(Tl208file["tree"]))     |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
-K40 = fill_from_root_file(K40file, "tree", keys(K40file["tree"]))           |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
-Pa234m = fill_from_root_file(Pa234mfile, "tree", keys(Pa234mfile["tree"]))  |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
-bb = fill_from_root_file(bbfile, "tree", keys(bbfile["tree"]))              |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
+Bi214 = fill_from_root_file(Bi214File, "tree", keys(Bi214File["tree"]))     |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
+Bi214Surf = fill_from_root_file(Bi214SurfFile, "tree", keys(Bi214File["tree"]))     |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
+Bi214Bulk = fill_from_root_file(Bi214BulkFile, "tree", keys(Bi214File["tree"]))     |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
+Tl208 = fill_from_root_file(Tl208File, "tree", keys(Tl208File["tree"]))     |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
+K40 = fill_from_root_file(K40File, "tree", keys(K40File["tree"]))           |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
+Pa234m = fill_from_root_file(Pa234mFile, "tree", keys(Pa234mFile["tree"]))  |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
+bb = fill_from_root_file(bbFile, "tree", keys(bbFile["tree"]))              |> add_vertex_2D_separation_column! |> add_vertex_dy_separation_column! |> add_vertex_dz_separation_column!
 
 binWidth = 1
 with(   
@@ -33,15 +37,17 @@ with(
         normed = :true,
 
     ) do
-    stephist(Bi214.d, c= 2,  label = "Bi214", lw = 3)
-    stephist!(Tl208.d, c = 3,  label = "Tl208", lw = 3)
-    stephist!(K40.d, c= 4, label = "K40", lw = 3)
-    stephist!(Pa234m.d, c=5, label = "Pa234m", lw = 3)
+    stephist(Bi214.d, c= 2,  label = "Bi214", lw = 4)
+    stephist!(Bi214Surf.d, c= 6,  label = "Bi214Surf", lw = 4)
+    stephist!(Bi214Bulk.d, c= 8,  label = "Bi214Bulk", lw = 4)
+    stephist!(Tl208.d, c = 3,  label = "Tl208", lw = 4)
+    stephist!(K40.d, c= 4, label = "K40", lw = 4)
+    stephist!(Pa234m.d, c=5, label = "Pa234m", lw = 4)
     stephist!(
         bb.d, 
         c =1,
         label = "bb", 
-        lw = 3, 
+        lw = 4, 
         fill = 0, 
         fa = 0.4, 
         fillstyle =:cross,
@@ -52,6 +58,7 @@ with(
     safesave(plotsdir("VertexSeparation", "1Dhist_VertexSeparation_d.png"), current())
     current()
 end
+
 
 with(   
         title = "1D Vertex separation; "* L"d_y = |y_1 - y_2|",
@@ -66,6 +73,8 @@ with(
 
     ) do
     stephist(Bi214.dy,  c= 2,label = "Bi214", lw = 3)
+    stephist!(Bi214Surf.dy,  c= 6,label = "Bi214Surf", lw = 3)
+    stephist!(Bi214Bulk.dy,  c= 8,label = "Bi214Bulk", lw = 3)
     stephist!(Tl208.dy,  c= 3, label = "Tl208", lw = 3)
     stephist!(K40.dy,  c=4, label = "K40", lw = 3)
     stephist!(Pa234m.dy, c=5 ,label = "Pa234m", lw = 3)
@@ -81,24 +90,24 @@ with(
         xlabel = "vertexSeparation/mm", 
         ylabel = "normalized rate/$binWidth mm"
     )
-    # safesave(plotsdir("VertexSeparation", "1Dhist_VertexSeparation_dy.png"), current())
+    safesave(plotsdir("VertexSeparation", "1Dhist_VertexSeparation_dy.png"), current())
     current()
 end
 
 with(   
         title = "1D Vertex separation; "* L"d_z = |z_1 - z_2|",
         legend = :best,
-        thickness_scaling = 2.0,
+        thickness_scaling = 1.4,
         lw = 4,
         right_margin=12Plots.mm,
         bottom_margin=4Plots.mm,
         size = (1200, 800),
         nbins = 0:binWidth:100,
         normed = :true,
-        frame = :true
-
     ) do
     stephist(Bi214.dz,  c= 2,label = "Bi214", lw = 3)
+    stephist!(Bi214Surf.dz,  c= 6,label = "Bi214Surf", lw = 3)
+    stephist!(Bi214Bulk.dz,  c= 8,label = "Bi214Bulk", lw = 3)
     stephist!(Tl208.dz,  c= 3, label = "Tl208", lw = 3)
     stephist!(K40.dz,  c=4, label = "K40", lw = 3)
     stephist!(Pa234m.dz, c=5,label = "Pa234m", lw = 3)
@@ -114,11 +123,11 @@ with(
         xlabel = "vertexSeparation/mm", 
         ylabel = "normalized rate/$binWidth mm"
     )
-    # safesave(plotsdir("VertexSeparation", "1Dhist_VertexSeparation_dz.png"), current())
+    safesave(plotsdir("VertexSeparation", "1Dhist_VertexSeparation_dz.png"), current())
     current()
 end
 
-quantile(bb.dy, 0.90)
+quantile(Bi214Surf.dy, 0.90)
 quantile(bb.dz, 0.90)
 
 with() do
@@ -138,18 +147,3 @@ with() do
     # safesave(plotsdir("VertexSeparation", "2D_Histogram_bb.png"), current())
     current()
 end
-
-()
-h(x, y) = exp(x^2 + y^2)
-
-x = range(-3, 3, length=100)
-y = range(-3, 3, length=100)
-z = @. h(x', y)
-
-tv = 0:8
-tl = [L"10^{%$i}" for i in tv]
-contourf(x, y, log10.(z), color=:turbo, levels=8,
-    colorbar_ticks=(tv, tl), aspect_ratio=:equal,
-    title=L"\exp(x^{2} + y^{2})", xlabel=L"x", ylabel=L"y")
-
-pgfplotsx()
