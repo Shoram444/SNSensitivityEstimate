429078f6f4ebb821f6a47390e791937cf8166e96
Shoram444 <petro.maros.afs@gmail.com>
2024-06-18 09:56:01 +0200

    plots together

--- scripts/ICHEP24/figures.jl
+++ scripts/ICHEP24/figures.jl
@@ -22,7 +22,7 @@ gdf = groupby(df, :E1)
 # make a projection onto E2 axis
 single_spectrum = [maximum(g.dGdE) for g in gdf] 
 with_theme(theme_latexfonts()) do 
-    f = Figure(size = (900, 600), fontsize = 36,  figure_padding = 36)
+    f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Single electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" , title = L"Theoretical single-electron energy spectrum $$", )
      
     p = lines!(a, single_spectrum, label = L"standard $2\nu\beta\beta$", color = orange, linewidth = 5)
@@ -46,7 +46,7 @@ sum_spectrum = [sum(g.dGdE) for g in gdf2]
 sum_spectrum ./= maximum(sum_spectrum)
 
 with_theme(theme_latexfonts()) do 
-    f = Figure(size = (900, 600), fontsize = 36,  figure_padding = 36)
+    f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Summed 2-electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" , title = L"Summed 2-electron energy spectrum for $2\nu\beta\beta$", )
      
     p = lines!(a, sum_spectrum, color = orange, linewidth = 5, label= L"standard $2\nu\beta\beta$")
@@ -63,7 +63,7 @@ h = Hist1D(thetas; binedges=0:5:180)
 ys = bincounts(h) ./ maximum(bincounts(h))
 
 with_theme(theme_latexfonts()) do 
-    f = Figure(size = (900, 600), fontsize = 36,  figure_padding = 36)
+    f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Angle between electrons $[^{\circ}]$", ylabel = L"Rate [a.u.] $$" , title = L"Theoretical angular distribution $$", )
      
     p = lines!(a, range(0,180, length(ys)),ys, color = orange, linewidth = 5, label = L"standard $2\nu\beta\beta$")
@@ -92,7 +92,7 @@ gdf_RH = groupby(df_RH, :E1)
 # make a projection onto E2 axis
 single_spectrum_RH = [maximum(g.dGdE) for g in gdf_RH] 
 with_theme(theme_latexfonts()) do 
-    f = Figure(size = (900, 600), fontsize = 36,  figure_padding = 36)
+    f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Single electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" , title = L"Theoretical single-electron energy spectrum $$", )
      
     p = lines!(a, range(-15,3000, length(single_spectrum)),single_spectrum, label = L"standard $2\nu\beta\beta$", color = orange, linewidth = 5)
@@ -117,7 +117,7 @@ sum_spectrum_RH = [sum(g.dGdE) for g in gdf2_RH]
 sum_spectrum_RH ./= maximum(sum_spectrum_RH)
 
 with_theme(theme_latexfonts()) do 
-    f = Figure(size = (900, 600), fontsize = 36,  figure_padding = 36)
+    f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Summed 2-electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" , title = L"Summed 2-electron energy spectrum for $2\nu\beta\beta$", )
      
     p = lines!(a,range(0,3000, length(sum_spectrum)) ,sum_spectrum, color = orange, linewidth = 5, label= L"standard $2\nu\beta\beta$")
@@ -140,7 +140,7 @@ h_RH = Hist1D(thetas_RH; binedges=0:5:180)
 ys_RH = bincounts(h_RH) ./ maximum(bincounts(h_RH))
 
 with_theme(theme_latexfonts()) do 
-    f = Figure(size = (900, 600), fontsize = 36,  figure_padding = 36)
+    f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Angle between electrons $[^{\circ}]$", ylabel = L"Rate [a.u.] $$" , title = L"Theoretical angular distribution $$", )
      
     p = lines!(a, range(0,180, length(ys)), ys, color = orange, linewidth = 5, label = L"standard $2\nu\beta\beta$")
@@ -155,4 +155,68 @@ with_theme(theme_latexfonts()) do
     f
 end
 
+
+## Together standard RH
+
+with_theme(theme_latexfonts()) do 
+    f = Figure(size = (750, 500), fontsize = 22,  figure_padding = 22)
+    Label(f[1, 1:2], "Theoretical predictions", tellwidth = false)
+    a1 = Axis(f[3,1],title= L"Energy spectrum $$", xlabel = L"Single electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" )
+    a2 = Axis(f[3,2],title= L"Angular distribution $$", xlabel = L"Angle between electrons $[^{\circ}]$", ylabel = L"Rate [a.u.] $$", yaxisposition = :right)
+     
+    p1 = lines!(a1,range(0,3000, length(sum_spectrum)) ,sum_spectrum, color = orange, linewidth = 3.5, label= L"standard $2\nu\beta\beta$")
+    p2 = lines!(a1,range(0,3000, length(sum_spectrum_RH)) ,sum_spectrum_RH, color = aqua, linewidth = 3.5, label= L"$2\nu\beta\beta$ with $\bar{\nu}_R$")
+
+    p3 = lines!(a2, range(0,180, length(ys)), ys, color = orange, linewidth = 3.5, label = L"standard $2\nu\beta\beta$")
+    p4 = lines!(a2, range(0,180, length(ys_RH)), ys_RH, color = aqua, linewidth = 3.5, label = L"$2\nu\beta\beta$ with $\bar{\nu}_R")
+
+    xlims!(a1, 0, 3000)
+    ylims!(a1, 0, 1.1)
+    xlims!(a2, 0, 180)
+    ylims!(a2, 0, 1.1)
+
+    a2.xticks= ([0,60, 120, 180])
+    hideydecorations!(a2, ticks = true, grid = false)
+    # axislegend(a, margin = (15, 15, 15, 15), tellwidth = true,patchsize = (35, 4), position=:cb)
+
+    Legend(f[2,1:2], [p1, p2], [L"standard $2\nu\beta\beta$",L"$2\nu\beta\beta$ with $\bar{\nu}_R$"], orientation = :horizontal, patchsize = (35, 4))
+    colgap!(f.layout, 1, Fixed(38))
+
+    save(scriptsdir("ICHEP24","RH_standard_together.png"), f)
+    save(scriptsdir("ICHEP24","RH_standard_together.svg"), f)
+    save(scriptsdir("ICHEP24","RH_standard_together.pdf"), f)
+
+    f
+end
+
+with_theme(theme_latexfonts()) do 
+    f = Figure(size = (750, 500), fontsize = 22,  figure_padding = 22)
+    Label(f[1, 1:2], "Theoretical predictions", tellwidth = false)
+    a1 = Axis(f[3,1],title= L"Energy spectrum $$", xlabel = L"Single electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" )
+    a2 = Axis(f[3,2],title= L"Angular distribution $$", xlabel = L"Angle between electrons $[^{\circ}]$", ylabel = L"Rate [a.u.] $$", yaxisposition = :right)
+     
+    p1 = lines!(a1,range(0,3000, length(sum_spectrum)) ,sum_spectrum, color = orange, linewidth = 3.5, label= L"standard $2\nu\beta\beta$")
+    # p2 = lines!(a1,range(0,3000, length(sum_spectrum_RH)) ,sum_spectrum_RH, color = aqua, linewidth = 3.5, label= L"$2\nu\beta\beta$ with $\bar{\nu}_R$")
+
+    p3 = lines!(a2, range(0,180, length(ys)), ys, color = orange, linewidth = 3.5, label = L"standard $2\nu\beta\beta$")
+    # p4 = lines!(a2, range(0,180, length(ys_RH)), ys_RH, color = aqua, linewidth = 3.5, label = L"$2\nu\beta\beta$ with $\bar{\nu}_R")
+
+    xlims!(a1, 0, 3000)
+    ylims!(a1, 0, 1.1)
+    xlims!(a2, 0, 180)
+    ylims!(a2, 0, 1.1)
+
+    a2.xticks= ([0,60, 120, 180])
+    hideydecorations!(a2, ticks = true, grid = false)
+    # axislegend(a, margin = (15, 15, 15, 15), tellwidth = true,patchsize = (35, 4), position=:cb)
+
+    Legend(f[2,1:2], [p1], [L"standard $2\nu\beta\beta$"], orientation = :horizontal, patchsize = (35, 4))
+    colgap!(f.layout, 1, Fixed(38))
+
+    save(scriptsdir("ICHEP24","standard_together.png"), f)
+    save(scriptsdir("ICHEP24","standard_together.svg"), f)
+    save(scriptsdir("ICHEP24","standard_together.pdf"), f)
+
+    f
+end
 
