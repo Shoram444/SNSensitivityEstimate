92cbf78ba2bd703fb96a1d5dfbf7a5bbfb575443
Shoram444 <petro.maros.afs@gmail.com>
2024-06-19 12:56:57 +0200

    updated colors

--- scripts/ICHEP24/figures.jl
+++ scripts/ICHEP24/figures.jl
@@ -8,9 +8,10 @@ Revise.track(SensitivityModule)
 
 include(scriptsdir("Params.jl"))
 
-orange = "#EF600A"
-aqua = "#66E9E6"
-dark = "#52BCBA"
+dark_orange = "#EF600A"
+light_orange = "#F3DB29"
+light_blue = "#2DC3EF"
+dark_blue = "#0D3B7C"
 
 ### Standard
 inPath = joinpath("/home/shoram/Work/PhD_Thesis/Data/Se82/1-Gfactors/spectrumG0.dat")
@@ -25,7 +26,7 @@ with_theme(theme_latexfonts()) do
     f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Single electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" , title = L"Theoretical single-electron energy spectrum $$", )
      
-    p = lines!(a, single_spectrum, label = L"standard $2\nu\beta\beta$", color = orange, linewidth = 5)
+    p = lines!(a, single_spectrum, label = L"standard $2\nu\beta\beta$", color = dark_orange, linewidth = 5)
     
     axislegend(a, margin = (15, 15, 15, 15), tellwidth = true,patchsize = (35, 4))
 
@@ -49,7 +50,7 @@ with_theme(theme_latexfonts()) do
     f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Summed 2-electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" , title = L"Summed 2-electron energy spectrum for $2\nu\beta\beta$", )
      
-    p = lines!(a, sum_spectrum, color = orange, linewidth = 5, label= L"standard $2\nu\beta\beta$")
+    p = lines!(a, sum_spectrum, color = dark_orange, linewidth = 5, label= L"standard $2\nu\beta\beta$")
 
     xlims!(a, 0, 3000)
     ylims!(a, 0, 1.1)
@@ -66,7 +67,7 @@ with_theme(theme_latexfonts()) do
     f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Angle between electrons $[^{\circ}]$", ylabel = L"Rate [a.u.] $$" , title = L"Theoretical angular distribution $$", )
      
-    p = lines!(a, range(0,180, length(ys)),ys, color = orange, linewidth = 5, label = L"standard $2\nu\beta\beta$")
+    p = lines!(a, range(0,180, length(ys)),ys, color = dark_orange, linewidth = 5, label = L"standard $2\nu\beta\beta$")
 
     xlims!(a, 0, 180)
     ylims!(a, 0, 1.1)
@@ -95,8 +96,8 @@ with_theme(theme_latexfonts()) do
     f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Single electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" , title = L"Theoretical single-electron energy spectrum $$", )
      
-    p = lines!(a, range(-15,3000, length(single_spectrum)),single_spectrum, label = L"standard $2\nu\beta\beta$", color = orange, linewidth = 5)
-    lines!(a, range(-15,3000, length(single_spectrum_RH)),single_spectrum_RH, label = L"$2\nu\beta\beta$ with $\bar{\nu}_R$", color = aqua, linewidth = 5)
+    p = lines!(a, range(-15,3000, length(single_spectrum)),single_spectrum, label = L"standard $2\nu\beta\beta$", color = dark_orange, linewidth = 5)
+    lines!(a, range(-15,3000, length(single_spectrum_RH)),single_spectrum_RH, label = L"$2\nu\beta\beta$ with $\bar{\nu}_R$", color = light_blue, linewidth = 5)
     
     axislegend(a, margin = (15, 15, 15, 15), tellwidth = true,patchsize = (35, 4))
 
@@ -120,8 +121,8 @@ with_theme(theme_latexfonts()) do
     f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Summed 2-electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" , title = L"Summed 2-electron energy spectrum for $2\nu\beta\beta$", )
      
-    p = lines!(a,range(0,3000, length(sum_spectrum)) ,sum_spectrum, color = orange, linewidth = 5, label= L"standard $2\nu\beta\beta$")
-    lines!(a,range(0,3000, length(sum_spectrum_RH)) ,sum_spectrum_RH, color = aqua, linewidth = 5, label= L"$2\nu\beta\beta$ with $\bar{\nu}_R$")
+    p = lines!(a,range(0,3000, length(sum_spectrum)) ,sum_spectrum, color = dark_orange, linewidth = 5, label= L"standard $2\nu\beta\beta$")
+    lines!(a,range(0,3000, length(sum_spectrum_RH)) ,sum_spectrum_RH, color = light_blue, linewidth = 5, label= L"$2\nu\beta\beta$ with $\bar{\nu}_R$")
 
     xlims!(a, 0, 3000)
     ylims!(a, 0, 1.1)
@@ -143,8 +144,8 @@ with_theme(theme_latexfonts()) do
     f = Figure(size = (750, 500), fontsize = 36,  figure_padding = 36)
     a = Axis(f[1,1], xlabel = L"Angle between electrons $[^{\circ}]$", ylabel = L"Rate [a.u.] $$" , title = L"Theoretical angular distribution $$", )
      
-    p = lines!(a, range(0,180, length(ys)), ys, color = orange, linewidth = 5, label = L"standard $2\nu\beta\beta$")
-    p = lines!(a, range(0,180, length(ys_RH)), ys_RH, color = aqua, linewidth = 5, label = L"$2\nu\beta\beta$ with $\bar{\nu}_R")
+    p = lines!(a, range(0,180, length(ys)), ys, color = dark_orange, linewidth = 5, label = L"standard $2\nu\beta\beta$")
+    p = lines!(a, range(0,180, length(ys_RH)), ys_RH, color = light_blue, linewidth = 5, label = L"$2\nu\beta\beta$ with $\bar{\nu}_R")
 
     xlims!(a, 0, 180)
     ylims!(a, 0, 1.1)
@@ -164,11 +165,11 @@ with_theme(theme_latexfonts()) do
     a1 = Axis(f[3,1],title= L"Energy spectrum $$", xlabel = L"Single electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" )
     a2 = Axis(f[3,2],title= L"Angular distribution $$", xlabel = L"Angle between electrons $[^{\circ}]$", ylabel = L"Rate [a.u.] $$", yaxisposition = :right)
      
-    p1 = lines!(a1,range(0,3000, length(sum_spectrum)) ,sum_spectrum, color = orange, linewidth = 3.5, label= L"standard $2\nu\beta\beta$")
-    p2 = lines!(a1,range(0,3000, length(sum_spectrum_RH)) ,sum_spectrum_RH, color = aqua, linewidth = 3.5, label= L"$2\nu\beta\beta$ with $\bar{\nu}_R$")
+    p1 = lines!(a1,range(0,3000, length(sum_spectrum)) ,sum_spectrum, color = dark_orange, linewidth = 3.5, label= L"standard $2\nu\beta\beta$")
+    p2 = lines!(a1,range(0,3000, length(sum_spectrum_RH)) ,sum_spectrum_RH, color = light_blue, linewidth = 3.5, label= L"$2\nu\beta\beta$ with $\bar{\nu}_R$")
 
-    p3 = lines!(a2, range(0,180, length(ys)), ys, color = orange, linewidth = 3.5, label = L"standard $2\nu\beta\beta$")
-    p4 = lines!(a2, range(0,180, length(ys_RH)), ys_RH, color = aqua, linewidth = 3.5, label = L"$2\nu\beta\beta$ with $\bar{\nu}_R")
+    p3 = lines!(a2, range(0,180, length(ys)), ys, color = dark_orange, linewidth = 3.5, label = L"standard $2\nu\beta\beta$")
+    p4 = lines!(a2, range(0,180, length(ys_RH)), ys_RH, color = light_blue, linewidth = 3.5, label = L"$2\nu\beta\beta$ with $\bar{\nu}_R")
 
     xlims!(a1, 0, 3000)
     ylims!(a1, 0, 1.1)
@@ -195,11 +196,11 @@ with_theme(theme_latexfonts()) do
     a1 = Axis(f[3,1],title= L"Energy spectrum $$", xlabel = L"Single electron energy [keV] $$", ylabel = L"Rate [a.u.] $$" )
     a2 = Axis(f[3,2],title= L"Angular distribution $$", xlabel = L"Angle between electrons $[^{\circ}]$", ylabel = L"Rate [a.u.] $$", yaxisposition = :right)
      
-    p1 = lines!(a1,range(0,3000, length(sum_spectrum)) ,sum_spectrum, color = orange, linewidth = 3.5, label= L"standard $2\nu\beta\beta$")
-    # p2 = lines!(a1,range(0,3000, length(sum_spectrum_RH)) ,sum_spectrum_RH, color = aqua, linewidth = 3.5, label= L"$2\nu\beta\beta$ with $\bar{\nu}_R$")
+    p1 = lines!(a1,range(0,3000, length(sum_spectrum)) ,sum_spectrum, color = dark_orange, linewidth = 3.5, label= L"standard $2\nu\beta\beta$")
+    # p2 = lines!(a1,range(0,3000, length(sum_spectrum_RH)) ,sum_spectrum_RH, color = light_blue, linewidth = 3.5, label= L"$2\nu\beta\beta$ with $\bar{\nu}_R$")
 
-    p3 = lines!(a2, range(0,180, length(ys)), ys, color = orange, linewidth = 3.5, label = L"standard $2\nu\beta\beta$")
-    # p4 = lines!(a2, range(0,180, length(ys_RH)), ys_RH, color = aqua, linewidth = 3.5, label = L"$2\nu\beta\beta$ with $\bar{\nu}_R")
+    p3 = lines!(a2, range(0,180, length(ys)), ys, color = dark_orange, linewidth = 3.5, label = L"standard $2\nu\beta\beta$")
+    # p4 = lines!(a2, range(0,180, length(ys_RH)), ys_RH, color = light_blue, linewidth = 3.5, label = L"$2\nu\beta\beta$ with $\bar{\nu}_R")
 
     xlims!(a1, 0, 3000)
     ylims!(a1, 0, 1.1)
